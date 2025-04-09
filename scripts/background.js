ff5830ffddc4cf783226ca87697fdfa7d69cd65b
dongpo.li <dongpo.li@hotmail.com>
2025-04-09 16:51:35 +0800

    添加安装后自动打开设置页面 删除icon,待定 点击插件按钮不再有反应

--- scripts/background.js
+++ scripts/background.js
@@ -1,2 +1,8 @@
 // 允许在content.js中使用chrome.storage.session
 chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
+
+chrome.runtime.onInstalled.addListener((_reason) => {
+    chrome.tabs.create({
+        url: 'pages/setting.html'
+    });
+});
